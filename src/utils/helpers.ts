52382c439fe39cc34b1f876eea93ee93e35cd2c9
Shazid <shazid.dev@gmail.com>
2024-01-15 13:48:05 +0600

    feat: count error and acuracy calculator helper added

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -11,3 +11,24 @@ export const isKeyboardCodeAllowed = (code: string) => {
     code === "Space"
   );
 };
+
+export const countErrors = (actual: string, expected: string) => {
+  const expectedCharacters = expected.split("");
+
+  return expectedCharacters.reduce((errors, expectedChar, i) => {
+    const actualChar = actual[i];
+    if (actualChar !== expectedChar) {
+      errors++;
+    }
+    return errors;
+  }, 0);
+};
+
+export const calculateAccuracyPercentage = (errors: number, total: number) => {
+  if (total > 0) {
+    const corrects = total - errors;
+    return (corrects / total) * 100;
+  }
+
+  return 0;
+};
